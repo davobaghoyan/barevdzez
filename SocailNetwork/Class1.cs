476c6ebed62a5f9499296cf73ca96771f6fc13c6
Davit Baghoyan <dbaghoyan@mail.ru>
2019-11-10 18:11:28 +0400

    Add project files.

---
+++ SocailNetwork/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocailNetwork
+{
+    class User
+    {
+        private string gender;
+        private  DateTime dateofbirth;
+        public string Name { get; set; }
+        public DateTime Dateofbirth
+        {
+            get { return this.Dateofbirth; }
+            set
+            {
+                if (this.Dateofbirth.Year < 2010)
+                {
+                    this.dateofbirth = value;
+                 }
+
+                else
+                {
+                    this.dateofbirth = DateTime.Now;
+                    this.dateofbirth = DateTime.Now;
+                }
+            }
+        }
+
+        public string Gender { get; set; }
+
+        public User ()
+        { }
+
+        public User (String name, DateTime dateofbirth,string gender)
+        {
+            this.Name = name;
+            this.Dateofbirth = dateofbirth;
+            this.Gender = gender;
+        }
+
+
+        public Profile Userprofile { get; set; }
+
+
+
+        public void Createprofile ()
+
+        {
+            this.Userprofile = new Profile();
+        }
+
+    }
+}
---
+++ SocailNetwork/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocailNetwork
+{
+    class Profile
+    {
+        public List<User> Friends;
+        public List <Interests> Interests { get; set; }
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User myUser = new User();
+            myUser.Name = "Anna";
+            myUser.Dateofbirth = new DateTime(2000, 10, 11);
+            myUser.Gender = "Female";
+
+            User johnuser = new User()
+            {
+                Name = " John",
+                Dateofbirth = new DateTime(2000, 11, 22),
+                Gender = "Male"
+
+            };
+
+            User jackuser = new User("Jack", new DateTime(2000, 5, 11), "Male");
+
+
+
+
+        }
+
+
+
+    }
+}
